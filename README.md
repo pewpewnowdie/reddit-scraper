3e65de5b4303d87fcd03d461b8786012627260c7
Kshitij Tyagi <kshitijtyagiwastaken@gmail.com>
2023-12-24 21:26:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# discord-meme-boy
+ meme gen
---
+++ main.py
@@ -0,0 +1,71 @@
+import requests
+
+def get_request(subreddit, next, time):
+    url = f"https://www.reddit.com/r/{subreddit}/top.json?t={time}{next}"
+
+    headers = {
+        "User-Agent": "pewpewnowdie"
+    }
+
+    response = requests.get(url, headers=headers)
+    return response
+
+def get_posts(subreddits, next = '', time = 'week'):
+    posts = []
+    if type(subreddits) == type([]):
+        for subreddit in subreddits:
+            response = get_request(subreddit, next, time)
+            response_json = response.json()
+            children = response_json['data']['children']
+            for i in range (len(children)):
+                post = {}
+                data = children[i]['data']
+                try:
+                    post['id'] = data['name']
+                    post['title'] = data['title'] 
+                    media = data['media']
+                    post['upvotes'] = data['ups']
+                    post['comments'] = data['num_comments']
+                    post['upvote_ratio'] = data['upvote_ratio']
+                    post['subreddit'] = data['subreddit_name_prefixed']
+                    post['source'] = 'https://www.rxddit.com'+data['permalink']
+                    if(media):
+                        post['dash'] = media['reddit_video']['dash_url']
+                    else:
+                        post['dash'] = media
+                except:
+                    break
+                posts.append(post)
+                
+    elif type(subreddits) == type(''):
+        response = get_request(subreddits, next, time)
+        response_json = response.json()
+        children = response_json['data']['children']
+        for i in range (len(children)):
+            post = {}
+            data = children[i]['data']
+            try:
+                post['id'] = data['name']
+                post['title'] = data['title'] 
+                media = data['media']
+                post['comments'] = data['num_comments']
+                post['upvote_ratio'] = data['upvote_ratio']
+                post['upvote_ratio'] = data['upvote_ratio']
+                post['subreddit'] = data['subreddit_name_prefixed']
+                post['source'] = 'https://www.rxddit.com'+data['permalink']
+                if(media):
+                    post['dash'] = media['reddit_video']['dash_url']
+                else:
+                    post['dash'] = media
+            except:
+                break
+            posts.append(post)
+    else:
+        print('argument should be a string')
+    return posts
+
+subreddits = ['IndianMeyMeys', 'IndianDankMemes']
+posts = get_posts(subreddits)
+
+for post in posts:
+    print(post['title'])
