dffe3de41ccb61775e5bbd59aba4be1969bb342b
Kshitij Tyagi <kshitijtyagiwastaken@gmail.com>
2023-12-24 23:11:40 +0530

    parse func, multiple post scraping
    added a seperate parse function from get_posts() to scrape 1 subreddit at a time, also added provision to scrape all the posts instead of first 20, made argument list of get_posts strictly a list and not a string

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
+import time
 
-def get_request(subreddit, next, time):
-    url = f"https://www.reddit.com/r/{subreddit}/top.json?t={time}{next}"
+def get_request(subreddit, param = '', dura = 'week'):
+    url = f"https://www.reddit.com/r/{subreddit}/top.json?t={dura}{param}"
 
     headers = {
         "User-Agent": "pewpewnowdie"
@@ -10,63 +11,48 @@ def get_request(subreddit, next, time):
     response = requests.get(url, headers=headers)
     return response
 
-def get_posts(subreddits, next = '', time = 'week'):
+def parse(subreddit, after = '', dura = 'week'):
     posts = []
-    if type(subreddits) == type([]):
-        for subreddit in subreddits:
-            response = get_request(subreddit, next, time)
-            response_json = response.json()
-            children = response_json['data']['children']
-            for i in range (len(children)):
-                post = {}
-                data = children[i]['data']
-                try:
-                    post['id'] = data['name']
-                    post['title'] = data['title'] 
-                    media = data['media']
-                    post['upvotes'] = data['ups']
-                    post['comments'] = data['num_comments']
-                    post['upvote_ratio'] = data['upvote_ratio']
-                    post['subreddit'] = data['subreddit_name_prefixed']
-                    post['source'] = 'https://www.rxddit.com'+data['permalink']
-                    if(media):
-                        post['dash'] = media['reddit_video']['dash_url']
-                    else:
-                        post['dash'] = media
-                except:
-                    break
-                posts.append(post)
-                
-    elif type(subreddits) == type(''):
-        response = get_request(subreddits, next, time)
-        response_json = response.json()
-        children = response_json['data']['children']
-        for i in range (len(children)):
-            post = {}
-            data = children[i]['data']
-            try:
-                post['id'] = data['name']
-                post['title'] = data['title'] 
-                media = data['media']
-                post['comments'] = data['num_comments']
-                post['upvote_ratio'] = data['upvote_ratio']
-                post['upvote_ratio'] = data['upvote_ratio']
-                post['subreddit'] = data['subreddit_name_prefixed']
-                post['source'] = 'https://www.rxddit.com'+data['permalink']
-                if(media):
-                    post['dash'] = media['reddit_video']['dash_url']
-                else:
-                    post['dash'] = media
-            except:
-                break
-            posts.append(post)
-    else:
-        print('argument should be a string')
+    response = get_request(subreddit, after, dura)
+    response_json = response.json()
+    children = response_json['data']['children']
+    for i in range (len(children)):
+        post = {}
+        data = children[i]['data']
+        try:
+            post['id'] = data['name']
+            post['title'] = data['title'] 
+            media = data['media']
+            post['upvotes'] = data['ups']
+            post['comments'] = data['num_comments']
+            post['upvote_ratio'] = data['upvote_ratio']
+            post['subreddit'] = data['subreddit_name_prefixed']
+            post['source'] = 'https://www.rxddit.com'+data['permalink']
+            if(media):
+                post['dash'] = media['reddit_video']['dash_url']
+            else:
+                post['dash'] = media
+        except:
+            break
+        posts.append(post)
+    after = response_json['data']['after']
+    if after:
+        param = '&after='+response_json['data']['after']
+        after_posts = parse(subreddit, param)
+        posts = posts + after_posts
+    return posts
+
+def get_posts(subreddits, after = '', dura = 'week'):
+    posts = []
+    for subreddit in subreddits:
+        temp = parse(subreddit, after, dura)
+        time.sleep(1)
+        posts = posts + temp
     return posts
 
 def main():
-    subreddits = ['IndianMeyMeys', 'IndianDankMemes']
+    subreddits = ['programming']
     posts = get_posts(subreddits)
+    print(len(posts))
 
-    for post in posts:
-        print(post['title'])
+main()
