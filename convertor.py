2aab374fba8c5e37831f5f83103a085d288cdd1d
Kshitij Tyagi <kshitijtyagiwastaken@gmail.com>
2023-12-26 14:47:59 +0530

    made audio and video file temp

--- convertor.py
+++ convertor.py
@@ -2,21 +2,30 @@
 # Import everything needed to edit video clips 
 from moviepy.editor import *
 import requests
+import tempfile
+
+def download_video(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with tempfile.NamedTemporaryFile(suffix='.mp4', delete=False) as temp_file:
+            temp_file.write(response.content)
+            return temp_file.name
+    else:
+        return None
 
 # loading video file
-response  = requests.get('https://v.redd.it/makrvr18628c1/DASH_720.mp4?source=fallback')
-with open('video.mp4', 'wb') as f:
-    f.write(response.content)
-clip = VideoFileClip('video.mp4')
+video_url = 'https://v.redd.it/makrvr18628c1/DASH_720.mp4?source=fallback'
+video_name = download_video(video_url)
+video_clip = VideoFileClip(video_name)
   
 # loading audio file 
-response = requests.get('https://v.redd.it/makrvr18628c1/DASH_AUDIO_64.mp4')
-with open('audio.mp4', 'wb') as f:
-    f.write(response.content)
-audioclip = AudioFileClip('audio.mp4')
+audio_url = 'https://v.redd.it/makrvr18628c1/DASH_AUDIO_64.mp4'
+audio_name = download_video(audio_url)
+audio_clip = AudioFileClip(audio_name)
   
 # adding audio to the video clip 
-videoclip = clip.set_audio(audioclip) 
+clip = video_clip.set_audio(audio_clip) 
 
-videoclip.write_videofile('final.mp4', audio = True)
-videoclip.preview()
+clip.write_videofile('final.mp4', audio = True)
+clip.preview()
+    
